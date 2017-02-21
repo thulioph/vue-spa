84b99657e1e3c022c021e952ac0604b7d44c02b4
Thulio Philipe <thulioph@gmail.com>
2017-02-21 16:21:20 -0300

    Adjusts the test flow.

--- karma.conf.js
+++ karma.conf.js
@@ -18,8 +18,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*spec.js',
-      'test/**/*spec.js'
+      'test/index.js'
     ],
 
 
@@ -31,7 +30,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'test/**/*spec.js': ['webpack']
+        'test/index.js': ['webpack']
     },
 
     // Using the webpack file
