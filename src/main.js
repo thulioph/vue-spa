687e3d94e55154b8acd042269ee1bf146012e7cf
Thulio Philipe <thulioph@gmail.com>
2017-02-21 12:32:03 -0300

    Adds the vue-router and set two new routes with own components.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './App.vue';
+import AppRecife from './Recife.vue';
+import AppPorto from './Porto.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/recife',
+    component: AppRecife
+  },
+  {
+    path: '/porto',
+    component: AppPorto
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
