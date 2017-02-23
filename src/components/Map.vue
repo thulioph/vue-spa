f107a27f2a45089847a7c80765bdba71930a92ea
thulioph <thulioph@gmail.com>
2017-02-23 03:30:25 -0300

    Adds the vue-google-maps as a dependencie.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,15 +2,29 @@
   <section>
     <div class="columns">
       <div class="map-container">
-        <h1>Mapa aqui!</h1>
+        <div style="width: 100%; height: 400px">
+          <google-map :center="center" :zoom="12"></google-map>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+  import { load, Map, Marker } from 'vue-google-maps';
+
+  load('AIzaSyAulRolyPLtvxwTJykxW6aILdohgLOQ-50');
+
   export default {
-    name: 'Map'
+    name: 'MapApp',
+    components: {
+      'googleMap': Map
+    },
+    data() {
+      return {
+        center: { lat: -16.4902128, lng: -39.0915235 }
+      }
+    }
   }
 </script>
 
