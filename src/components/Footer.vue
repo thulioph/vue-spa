ca9b80be2e58cdd214cb362ca369f7e7ce7971ff
thulioph <thulioph@gmail.com>
2017-02-23 21:03:32 -0300

    Adds the footer menu.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <footer class="footer"></footer>
-  </div>
+  <footer class="footer">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <p>© Caldinho do Nenen 2017 - All rights reserved.</p>
+        </div>
+
+        <div class="column">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">O Caldinho</a></li>
+            <li><a href="#">Cardápio</a></li>
+            <li><a href="#">Caldinho 360º </a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -11,7 +27,35 @@
 </script>
 
 <style lang="scss">
+  $gray: #888888;
+  $white: #FFFFFF;
+
   .footer {
     background-color: #000;
+    padding: 3rem 1.5rem 2rem;
+
+    p, a {
+      color: $gray;
+    }
+
+    .column {
+      &:last-child {
+        text-align: right;
+      }
+    }
+
+    li {
+      display: inline-block;
+      margin: 0 5px;
+      text-transform: uppercase;
+    }
+
+    a {
+      transition: color, .25s, linear;
+
+      &:hover {
+        color: $white;
+      }
+    }
   }
 </style>
